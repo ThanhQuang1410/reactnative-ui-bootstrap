bd2f300461dcf57af516002f876bc58cf190c722
ThanhQuang1410 <ttquang1410@gmail.com>
2019-07-13 11:44:00 +0700

    update project structure

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Div from './Div';
+import Button from './Button';
+import Card from './Card';
+import Input from './Input';
+import Gradient from './Gradient';
+import A from './A';
+import P from './P';
+import { useBootstrap, BootstrapProvider } from './theme';
+import {styleBootstrap, theme} from "./theme/bootstrap";
+
+export {
+    Div,
+    Button,
+    Card,
+    Input,
+    Gradient,
+    A,
+    P,
+    styleBootstrap,
+    theme,
+    useBootstrap,
+    BootstrapProvider
+};
